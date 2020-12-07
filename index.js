4eac1e1446af26160e346e8f30738875e843f9f1
Spectaz <75622069+Spectaz@users.noreply.github.com>
2020-12-07 19:36:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.once('ready', () => {console.log('Ready!');});
+client.login('NzczODYxODAzMjUxNzI4NDI1.X6PY9w.zPzhXHl_DNTP23RrOoa_BFcY0m0');
